cb557df8b8a2240699500251c88538254b4d7ed9
SergejKolesnik <rjktcybr@gmail.com>
2026-07-09 10:24:06 +0300

    Unify document files and editable checklists

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "camino-del-norte-2026-v10-linked-timeline-tickets";
+const CACHE_NAME = "camino-del-norte-2026-v11-document-sync";
 const APP_SHELL = [
   "./",
   "./index.html",
